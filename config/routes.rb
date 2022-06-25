baca6cf02bea451523ea9a71bee226ae077d5ac2
paroksetynka <108095086+paroksetynka@users.noreply.github.com>
2022-06-25 15:27:03 +0000

    add index show and new page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  root "articles#index" 
+  resources :articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get "/articles", to: "articles#index"
+  get "/articles/:id", to: "articles#show"
   # Defines the root path route ("/")
   # root "articles#index"
 end
+
